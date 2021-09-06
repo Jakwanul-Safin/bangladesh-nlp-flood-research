92135e15ee759a2d50815db777cd6af9ec940e24
Jakwanul-Safin <js5142@columbia.edu>
2021-09-05 20:37:23 -0400

    Moved files of bengali classifer into subdirectory and added geoparser files

---
+++ bengali_nlp/bengali_classifier/geoparser.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractclassmethod
+from collections import defaultdict
+from ..bengali_tools.locations import get_locations
+
+class BengaliGeoParser(ABC):
+    def __init__(self, locations = None):
+        if locations is None:
+            locations = get_locations()
+
+        self.upazilas = locations['upazilas']
+        self.districts = locations['districts']
+        self.divisions = locations['divisions'] 
+
+    @abstractclassmethod
+    def locate(self, tokens):
+        raise NotImplementedError()
+
+class OccurrenceGeoParser(BengaliGeoParser):
+    def locate(self, tokens, level = 0, thres = 30, USE_MAX_OCCURING = False):
+        div, dis, upa = self.votes(tokens)
+        if level == 0:
+            n = sum(div.values())
+
+        if USE_MAX_OCCURING:
+            return max(div.keys(), key = lambda k: div[k])
+
+        return tuple(k for k in div.keys() if div[k]/n *100 > thres)
+    
+    def votes(self, tokens):
+        division_flooding = defaultdict(int)
+        district_flooding = defaultdict(int)
+        upazila_flooding = defaultdict(int)
+
+        for tk in tokens:
+            if tk in self.districts:
+                division_flooding[self.districts[tk].split(".")[0]] += 1
+                district_flooding[self.districts[tk]] += 1
+                
+                for k, upazila in self.upazilas.items():
+                    district = ".".join(upazila.split(".")[:2])
+                    if district == self.districts[tk]:
+                        upazila_flooding[upazila] += 1
+
+            elif tk in self.upazilas:
+                division_flooding[self.upazilas[tk].split(".")[0]] += 1
+                district_flooding[".".join(self.upazilas[tk].split(".")[:2])] += 1
+                upazila_flooding[self.upazilas[tk]] += 1
+
+        return division_flooding, district_flooding, upazila_flooding
